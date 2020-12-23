26539a289900f1e9cdf403f8867c891480945e6c
Jason McGinty <jason_mcginty@yahoo.com>
2020-12-23 13:57:09 -0600

    pokedex completed

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+import './Pokecard.css';
+
+const Pokecard = (props) => {
+    const pokemon = props.pokemon
+    pokemon.img_src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+    return (
+        <div className="Pokecard">
+            <h3 className="name">{pokemon.name}</h3>
+            <img src={pokemon.img_src} alt={pokemon.name} class="image" />
+            <p className="details">Type: {pokemon.type}</p>
+            <p className="details">EXP: {pokemon.base_experience}</p>
+        </div>
+    )
+}
+
+export default Pokecard;
